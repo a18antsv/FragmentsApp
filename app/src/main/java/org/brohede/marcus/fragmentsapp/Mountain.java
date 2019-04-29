730b1f89aa31ef39e06dadd4ec728e8fa058d149
a18antsv <gotneifhd2@gmail.com>
2019-04-29 16:48:45 +0200

    Implemented methods and attributes in Mountain class.

--- app/src/main/java/org/brohede/marcus/fragmentsapp/Mountain.java
+++ app/src/main/java/org/brohede/marcus/fragmentsapp/Mountain.java
@@ -1,7 +1,48 @@
 package org.brohede.marcus.fragmentsapp;
 
 public class Mountain {
+    private int id;
+    private String name;
+    private int height;
+    private String location;
+    private String imgURL;
+    private String articleURL;
 
-    // You need to create proper member variables, methods, and constructors
+    public Mountain(int id, String name, int height, String location, String imgURL, String articleURL) {
+        this.id = id;
+        this.name = name;
+        this.height = height;
+        this.location = location;
+        this.imgURL = imgURL;
+        this.articleURL = articleURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public String getArticleURL() {
+        return articleURL;
+    }
+
+    public String getInfo() {
+        return name + " is part of the " + location + " mountain range and is " + height + "m high.";
+    }
 
 }
